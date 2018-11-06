bdab0545855792d375e04412c60bfc92b2c55479
SaahilChawande <chawandesaahil600@gmail.com>
2018-11-06 22:10:44 +0530

    Added list for displaying ingredients

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {RecipePage} from "../pages/recipe/recipe";
 import {RecipesPage} from "../pages/recipes/recipes";
 import {ShoppingListPage} from "../pages/shopping-list/shopping-list";
 import {TabsPage} from "../pages/tabs/tabs";
+import {ShoppingListService} from "../services/shopping-list";
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import {TabsPage} from "../pages/tabs/tabs";
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ShoppingListService
   ]
 })
 export class AppModule {}
--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import {NgForm} from "@angular/forms";
+import {ShoppingListService} from "../../services/shopping-list";
+import {Ingredient} from "../../models/ingredient";
 
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
-  constructor() {}
+  ingredients: Ingredient[];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   onAddItem(form: NgForm) {
-    console.log(form);
+    this.shoppingListService.addItem(form.value.ingredientName, form.value.amount);
+    form.reset();
+    console.log(this.shoppingListService.getItems());
+    this.loadItems();
+  }
+
+  ionViewWillEnter()  {
+    this.loadItems();
+  }
+
+  private loadItems() {
+    this.ingredients = this.shoppingListService.getItems();
   }
 
 }
