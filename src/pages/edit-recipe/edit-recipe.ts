e3ab82560cfcb9d82d4d42b1c4acb12546f30ccf
SaahilChawande <chawandesaahil600@gmail.com>
2018-11-09 15:36:02 +0530

    Added toasts for user feedback

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActionSheetController, AlertController, NavParams} from "ionic-angular";
+import {ActionSheetController, AlertController, NavParams, ToastController} from "ionic-angular";
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
@@ -11,7 +11,11 @@ export class EditRecipePage implements OnInit{
   selectOptions = ['Easy', 'Medium', 'Hard'];
   recipeForm: FormGroup;
 
-  constructor(private navParams: NavParams, private actionSheetController: ActionSheetController, private alertCtrl: AlertController) {}
+  constructor(private navParams: NavParams,
+              private actionSheetController: ActionSheetController,
+              private alertCtrl: AlertController,
+              private toastCtrl: ToastController
+  ) {}
 
   ngOnInit()  {
     this.mode = this.navParams.get('mode');
@@ -42,6 +46,7 @@ export class EditRecipePage implements OnInit{
               for (let i = len - 1; i >=0; i--) {
                 formArray.removeAt(i);
               }
+              this.showToast('Removed all', 1500, 'bottom');
             }
           }
         },
@@ -72,9 +77,11 @@ export class EditRecipePage implements OnInit{
           text: 'Add',
           handler: data => {
             if (data.name.trim() == '' || data.name == null)  {
+              this.showToast('Please enter a valid value', 1500, 'bottom');
               return;
             }
             (<FormArray>this.recipeForm.get('ingredients')).push(new FormControl(data.name, Validators.required));
+            this.showToast('Ingredient Added', 1500, 'bottom');
           }
         }
       ]
@@ -88,5 +95,14 @@ export class EditRecipePage implements OnInit{
       'difficulty': new FormControl('Medium', Validators.required),
       'ingredients': new FormArray([])
     });
+  }
+
+  private showToast(message: string, duration: number, position: string)  {
+    const toast = this.toastCtrl.create({
+      message: message,
+      duration: duration,
+      position: position
+    });
+    toast.present();
   }
 }
