ec6f7e1dacfd5c69a41e5c715cde3d2d80193034
SaahilChawande <chawandesaahil600@gmail.com>
2018-11-09 16:04:43 +0530

    Added model and service for recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {RecipesPage} from "../pages/recipes/recipes";
 import {ShoppingListPage} from "../pages/shopping-list/shopping-list";
 import {TabsPage} from "../pages/tabs/tabs";
 import {ShoppingListService} from "../services/shopping-list";
+import {RecipesService} from "../services/recipes";
 
 @NgModule({
   declarations: [
@@ -38,7 +39,8 @@ import {ShoppingListService} from "../services/shopping-list";
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    ShoppingListService
+    ShoppingListService,
+    RecipesService
   ]
 })
 export class AppModule {}
