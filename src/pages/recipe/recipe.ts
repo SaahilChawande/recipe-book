0e7ee867a10fc4f999689dd1e619a78d9da50de1
SaahilChawande <chawandesaahil600@gmail.com>
2018-11-09 17:51:47 +0530

    Added add ingredients to shopping list functionality

--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../../models/recipe";
 import {NavController, NavParams} from "ionic-angular";
 import {EditRecipePage} from "../edit-recipe/edit-recipe";
+import {ShoppingListService} from "../../services/shopping-list";
+import {RecipesService} from "../../services/recipes";
 
 @Component({
   selector: 'page-recipe',
@@ -11,14 +13,20 @@ export class RecipePage implements OnInit{
   recipe: Recipe;
   index: number;
 
-  constructor(private navCtrl: NavController, private navParams: NavParams) {}
+  constructor(private navCtrl: NavController,
+              private navParams: NavParams,
+              private shoppingListService: ShoppingListService,
+              private recipesService: RecipesService
+  ) {}
 
   ngOnInit()  {
     this.recipe = this.navParams.get('recipe');
     this.index = this.navParams.get('index');
   }
 
-  onAddIngredients()  {}
+  onAddIngredients()  {
+    this.shoppingListService.addItems(this.recipe.ingredients);
+  }
 
   onEditRecipe()  {
     this.navCtrl.push(EditRecipePage, {mode: 'Edit', recipe: this.recipe, index: this.index});
