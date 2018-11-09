0df297f14284cd02ed84fc654103379d85a9b7dd
SaahilChawande <chawandesaahil600@gmail.com>
2018-11-09 13:19:04 +0530

    Added action sheet and alert for managing ingredients

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {NavParams} from "ionic-angular";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ActionSheetController, AlertController, NavParams} from "ionic-angular";
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'page-edit-recipe',
@@ -11,7 +11,7 @@ export class EditRecipePage implements OnInit{
   selectOptions = ['Easy', 'Medium', 'Hard'];
   recipeForm: FormGroup;
 
-  constructor(private navParams: NavParams) {}
+  constructor(private navParams: NavParams, private actionSheetController: ActionSheetController, private alertCtrl: AlertController) {}
 
   ngOnInit()  {
     this.mode = this.navParams.get('mode');
@@ -22,11 +22,65 @@ export class EditRecipePage implements OnInit{
     console.log(this.recipeForm);
   }
 
+  onManageIngredients() {
+    const actionSheet = this.actionSheetController.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Add Ingredients',
+          handler: () =>  {
+            this.createNewIngredientAlert().present();
+          }
+        },
+        {
+          text: 'Remove all ingredients',
+          role: 'destructive',
+          handler: () => {
+
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  private createNewIngredientAlert()  {
+    return this.alertCtrl.create({
+      title: 'Add Ingredient',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            if (data.name.trim() == '' || data.name == null)  {
+              return;
+            }
+            (<FormArray>this.recipeForm.get('ingredients')).push(new FormControl(data.name, Validators.required));
+          }
+        }
+      ]
+    })
+  }
+
   private initializeForm()  {
     this.recipeForm = new FormGroup({
       'title': new FormControl(null, Validators.required),
       'description': new FormControl(null, Validators.required),
-      'difficulty': new FormControl('Medium', Validators.required)
+      'difficulty': new FormControl('Medium', Validators.required),
+      'ingredients': new FormArray([])
     });
   }
 }
