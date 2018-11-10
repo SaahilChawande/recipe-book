f06fbc51a492d25d58605ce713e6a343c3c8e6cd
SaahilChawande <chawandesaahil600@gmail.com>
2018-11-10 18:20:27 +0530

    Added signin form

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,15 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {NgForm} from "@angular/forms";
 
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html',
