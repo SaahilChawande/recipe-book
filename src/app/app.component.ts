e1cec0745298325a87af203be92b4226bb0da156
SaahilChawande <chawandesaahil600@gmail.com>
2018-11-12 14:52:18 +0530

    Added logout user functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,21 @@ import {TabsPage} from "../pages/tabs/tabs";
 import {SigninPage} from "../pages/signin/signin";
 import {SignupPage} from "../pages/signup/signup";
 import firebase from 'firebase';
+import {AuthService} from "../services/auth";
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  tabsPage:any = TabsPage;
+  rootPage:any = TabsPage;
   signinPage = SigninPage;
   signupPage = SignupPage;
   isAuthenticated = false;
 
   @ViewChild('nav') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController, private authService: AuthService) {
     firebase.initializeApp({
       apiKey: "AIzaSyDWxDuv-1BrfwsU52lov1aPqaWxv9h-s5w",
       authDomain: "ionic2-recipebook-79abe.firebaseapp.com"
@@ -27,10 +28,10 @@ export class MyApp {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.isAuthenticated = true;
-        this.nav.setRoot(this.tabsPage);
+        this.rootPage = TabsPage;
       } else {
         this.isAuthenticated = false;
-        this.nav.setRoot(this.signinPage);
+        this.rootPage = SigninPage;
       }
     });
     platform.ready().then(() => {
@@ -46,6 +47,9 @@ export class MyApp {
     this.menuCtrl.close();
   }
 
-  onLogout()  {}
+  onLogout()  {
+    this.authService.logout();
+    this.menuCtrl.close();
+  }
 }
 
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -8,4 +8,8 @@ export class AuthService  {
   signin(email: string, password: string) {
     return firebase.auth().signInWithEmailAndPassword(email, password);
   }
+
+  logout()  {
+    firebase.auth().signOut();
+  }
 }
